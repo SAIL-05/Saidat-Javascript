d5e756416905b650a77a038b84eb53a5f1542db0
Saidat01 <106707223+Saidat01@users.noreply.github.com>
2025-05-02 23:07:46 +0100

    ass

---
+++ script.js
@@ -0,0 +1,59 @@
+//variable declarator
+let myAge = 24
+console.log(myAge);
+const mySchool = "unilag"
+console.log(mySchool);
+//primitive data types
+//strings
+let wendy = "a determined lady"
+console.log(wendy);
+
+const school = "Babcock"
+console.log(school);
+
+//number
+const schoolFee = 1000
+console.log(schoolFee);
+
+let myNum = 250
+console.log(myNum);
+
+//boolean
+let iAmAgirl = true;
+let iAmAboy = false;
+console.log( iAmAgirl);
+console.log(iAmAboy);
+
+//undefined 
+var age =
+console.log(age);
+let love;
+console.log(love);
+
+//null
+let test2 = null
+console.log(test2);
+let bag = null
+console.log(bag);
+
+// // Non-primitive data types
+// // objects
+const student = {
+    name: "Saidat",
+    age: 24,
+    lovesCoding: true,
+    school: "sail innovation lab",
+    bestFood: "rice",
+};
+console.log(student);
+
+// // arrays
+const myArray = ["beebah", "wande", 234, false, { name: "dupe" }, [2, 4, 6]];
+console.log(myArray);
+
+
+
+
+
+
+
